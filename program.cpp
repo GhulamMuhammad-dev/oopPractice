b5d516830db14360e797b456eba97ee2c5a0cc30
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-03-27 20:36:41 +0500

    practicing array pointers

--- program.cpp
+++ program.cpp
@@ -1,113 +1,132 @@
+// #include<iostream>
+// #include<string>
+// #include<conio.h>
+// using namespace std;
+
+// class Car{
+
+//   private:
+//   int gear;
+//   int speed;
+//   bool carStart;
+//   bool breakApply;
+//   int currentPosition=0;
+//   char moveForward;
+
+//   public:
+
+//   Car(){};
+//   Car(int gear,int speed,bool carStart,bool breakApply){
+
+//     this->gear=gear;
+//     this->speed=speed;
+//     this->carStart=carStart;
+//     this->breakApply=breakApply;
+//   }
+
+//   void startCar(bool carStart){
+//      this->carStart=carStart;
+//   }
+
+//   void setGear(int gear){
+//     if(gear>0 && gear<5){
+//      this->gear=gear;
+//     }
+//     else{
+//         cout<<"wrong gear selected"<<endl;
+//     }
+//   }
+
+
+
+//   void driveCar(){
+
+//     if(carStart){
+      
+//       if(gear==1){
+//         this->speed=5;
+//       }
+//       else if(gear==2){
+//         this->speed=10;
+//       }
+//      else if(gear==3){
+//         this->speed=15;
+//       }
+//      else if(gear==4){
+//         this->speed=20;
+//       }
+
+//     }
+
+//     else{
+//         cout<<"Please start the car first"<<endl;
+//     }
+
+//   }
+
+
+//   void moveCar(){
+
+//       if(carStart && gear){
+
+//        do{
+//         moveForward=_getch();
+
+//         if(moveForward=='w'){
+//           currentPosition+=speed;
+//           cout<<currentPosition<<endl;
+//         }
+//         else if(moveForward=='s'){
+//           break;
+//         }
+
+
+
+//        }while(true);
+
+//       }
+
+
+
+
+//   }
+
+
+
+
+
+
+
+
+// };
+
+
+// int main(){
+
+// Car c1;
+// c1.startCar(true);
+// c1.setGear(2);
+// c1.driveCar();
+// c1.moveCar();
+
+
+
+
+
+
+
+
+//     return 0;
+// }
+
+
 #include<iostream>
 #include<string>
-#include<conio.h>
 using namespace std;
-
-class Car{
-
-  private:
-  int gear;
-  int speed;
-  bool carStart;
-  bool breakApply;
-  int currentPosition=0;
-  char moveForward;
-
-  public:
-
-  Car(){};
-  Car(int gear,int speed,bool carStart,bool breakApply){
-
-    this->gear=gear;
-    this->speed=speed;
-    this->carStart=carStart;
-    this->breakApply=breakApply;
-  }
-
-  void startCar(bool carStart){
-     this->carStart=carStart;
-  }
-
-  void setGear(int gear){
-    if(gear>0 && gear<5){
-     this->gear=gear;
-    }
-    else{
-        cout<<"wrong gear selected"<<endl;
-    }
-  }
-
-
-
-  void driveCar(){
-
-    if(carStart){
-      
-      if(gear==1){
-        this->speed=5;
-      }
-      else if(gear==2){
-        this->speed=10;
-      }
-     else if(gear==3){
-        this->speed=15;
-      }
-     else if(gear==4){
-        this->speed=20;
-      }
-
-    }
-
-    else{
-        cout<<"Please start the car first"<<endl;
-    }
-
-  }
-
-
-  void moveCar(){
-
-      if(carStart && gear){
-
-       do{
-        moveForward=_getch();
-
-        if(moveForward=='w'){
-          currentPosition+=speed;
-          cout<<currentPosition<<endl;
-        }
-        else if(moveForward=='s'){
-          break;
-        }
-
-
-
-       }while(true);
-
-      }
-
-
-
-
-  }
-
-
-
-
-
-
-
-
-};
-
-
 int main(){
 
-Car c1;
-c1.startCar(true);
-c1.setGear(2);
-c1.driveCar();
-c1.moveCar();
+string names[5]={"ahmad","ali","danyal","suama","bilal"};
+cout<<*names<<endl;
 
 
 
@@ -115,6 +134,5 @@ c1.moveCar();
 
 
 
-
-    return 0;
+  return 0;
 }
