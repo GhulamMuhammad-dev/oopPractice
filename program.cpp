28c77e3cd9fe534618833e1521e286af0c2d9ddd
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-03-31 22:53:49 +0500

    my second commit for today

--- program.cpp
+++ program.cpp
@@ -5,80 +5,138 @@
 #include<cstdlib>
 using namespace std;
 
-void clearScreen(){
-    system("cls");
+// void clearScreen(){
+//     system("cls");
+// }
+
+// class GameArea{
+//      protected:
+//      char Area[10][10];
+//      public:
+//        GameArea(){
+//         for(int i=0;i<10;i++){
+//             for(int j=0;j<10;j++){
+//                 Area[i][j]='_';
+//             }
+//         }
+
+
+//         for(int i=0;i<10;i++){
+//             for(int j=0;j<10;j++){
+//                cout<<Area[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//        }
+
+
+// };
+
+// class Star: public GameArea{
+
+//       protected:
+//      const char star='*';
+
+//      public:
+     
+//      void goRandom(){
+
+//             do{
+
+//     srand(static_cast<unsigned>( time(0)));
+//     int randomNum=rand()%7;
+//      char option;
+//      cout<<"Enter a to generate random number"<<endl;
+//      cin>>option;
+
+//      if(option=='a'){
+//          clearScreen();
+//         cout<<randomNum<<endl;
+//         GameArea g1;
+//      }
+//      else{
+//         break;
+//      }
+
+
+
+//     }while(true);
+
+//      }
+
+
+
+
+
+
+// };
+
+
+
+
+
+// int main(){
+//     Star s1;
+//     s1.goRandom();
+   
+
+
+
+
+
+//     return 0;
+// }
+
+void showArea(char Area[][10]){
+     for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+               cout<<Area[i][j]<<" ";
+            }
+            cout<<endl;
+        }
 }
 
-class GameArea{
-     protected:
-     char Area[10][10];
-     public:
-       GameArea(){
-        for(int i=0;i<10;i++){
+int main(){
+
+    char Area[10][10];
+     
+
+
+
+
+       for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
                 Area[i][j]='_';
             }
         }
 
 
-        for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-               cout<<Area[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-       }
-
-
-};
-
-class Star: public GameArea{
-
-      protected:
-     const char star='*';
-
-     public:
      
-     void goRandom(){
-
-            do{
-
-    srand(static_cast<unsigned>( time(0)));
-    int randomNum=rand()%7;
-     char option;
-     cout<<"Enter a to generate random number"<<endl;
-     cin>>option;
-
-     if(option=='a'){
-         clearScreen();
-        cout<<randomNum<<endl;
-        GameArea g1;
-     }
-     else{
-        break;
-     }
 
 
-
-    }while(true);
-
-     }
+      do{
+        char option;
+         cout<<"Enter a to play"<<endl;
+         cin>>option;
+        if(option=='a'){
+            system("cls");
+          showArea(Area);
+        }
+        else{
+            break;
+        }
 
 
 
 
 
 
-};
 
 
 
 
 
-int main(){
-    Star s1;
-    s1.goRandom();
-   
+      }while(true);
 
 
 
