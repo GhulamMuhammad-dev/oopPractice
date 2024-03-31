ecad0a3c5b98d8ece8acb2ef844e2aee8fc3681b
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-03-31 23:59:32 +0500

    another commit

--- program.cpp
+++ program.cpp
@@ -87,67 +87,96 @@ using namespace std;
 //     return 0;
 // }
 
-void showArea(char Area[][10]){
-     for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-               cout<<Area[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-}
+// void showArea(char Area[][10]){
+//      for(int i=0;i<10;i++){
+//             for(int j=0;j<10;j++){
+//                cout<<Area[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+// }
 
 
-void updateArea(char Area[][10],int ranNum){
-     for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-                if(j==ranNum && i==ranNum){
-                    Area[i][j]='*';
-                }
-                else{
-               Area[i][j]='_';
-                }
-            }
+// void updateArea(char Area[][10],int ranNum){
+//      for(int i=0;i<10;i++){
+//             for(int j=0;j<10;j++){
+//                 if(j==ranNum && i==ranNum){
+//                     Area[i][j]='*';
+//                 }
+//                 else{
+//                Area[i][j]='_';
+//                 }
+//             }
             
-        }
+//         }
+// }
+
+
+
+// int main(){
+
+//     char Area[10][10];
+//      char option;
+
+
+//       do{
+//       srand(static_cast<unsigned>( time(0)));
+//       int randomNum=rand()%7;
+//          cout<<"Enter a to play"<<endl;
+//          cin>>option;
+//         if(option=='a'){
+//             system("cls");
+//               showArea(Area);
+//             updateArea(Area,randomNum);
+//         }
+//         else{
+//             break;
+//         }
+
+
+
+
+
+
+
+
+
+
+
+//       }while(true);
+
+
+
+
+
+//     return 0;
+// }
+
+void fillAndAccessArray(int arr[], int size) {
+    // Fill the array with some values
+    for (int i = 0; i < size; ++i) {
+        arr[i] = i * 2; // Just an example, you can put any values here
+    }
+
+    // Access the values of the array and print them
+    std::cout << "Values of the array inside the function:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << "arr[" << i << "] = " << arr[i] << std::endl;
+    }
 }
 
+int main() {
+    const int size = 5;
+    int myArray[size];
 
+    // Call the function and pass the array and its size
+    fillAndAccessArray(myArray, size);
 
-int main(){
-
-    char Area[10][10];
-     char option;
-
-
-      do{
-      srand(static_cast<unsigned>( time(0)));
-      int randomNum=rand()%7;
-         cout<<"Enter a to play"<<endl;
-         cin>>option;
-        if(option=='a'){
-            system("cls");
-              showArea(Area);
-            updateArea(Area,randomNum);
-        }
-        else{
-            break;
-        }
-
-
-
-
-
-
-
-
-
-
-
-      }while(true);
-
-
-
-
+    // Accessing the array values in the main function
+    std::cout << "\nValues of the array in the main function:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << "myArray[" << i << "] = " << myArray[i] << std::endl;
+    }
 
     return 0;
 }
