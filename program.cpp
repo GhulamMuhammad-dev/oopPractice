87f2bbf96337314042aff9e3e9b50cc37e2b84b6
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-04-04 22:35:50 +0500

    this is the first commit of today

--- program.cpp
+++ program.cpp
@@ -287,3 +287,42 @@
 
 //     return 0;
 // }
+
+#include<iostream>
+#include<stdlib.h>
+#include<iomanip>
+
+using namespace std;
+void fun() 
+{ 
+    for (int i=0; i<10; i++) 
+    { 
+    } 
+} 
+ 
+int main() 
+{ 
+    /* Time function returns the time since the 
+        Epoch(jan 1 1970). Returned time is in seconds. */
+    time_t start, end; 
+ 
+    /* You can call it like this : start = time(NULL); 
+    in both the way start contain total time in seconds 
+    since the Epoch. */
+    time(&start); 
+ 
+    // unsync the I/O of C and C++. 
+    ios_base::sync_with_stdio(false); 
+ 
+    fun(); 
+ 
+    // Recording end time. 
+    time(&end); 
+ 
+    // Calculating total time taken by the program. 
+    double time_taken = double(end - start); 
+    cout << "Time taken by program is : " << fixed 
+        << time_taken << setprecision(5); 
+    cout << " sec " << endl; 
+    return 0; 
+} 
