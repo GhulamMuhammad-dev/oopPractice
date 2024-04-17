0ef0812bab31a1699926be9ded760da01bdf1bb9
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-04-17 22:44:08 +0500

    The first commit of today

--- program.cpp
+++ program.cpp
@@ -1,59 +1,78 @@
+// #include<iostream>
+// #include<string>
+// #include<vector>
+// using namespace std;
+
+
+// class GameArea{
+//      private:
+//      vector<vector<char>> grid;
+//      int size;
+//      char gameGrid[];
+//      public:
+//      GameArea(){
+//       cout<<"Enter the size of area"<<endl;
+//       cin>>size;
+//       grid=vector<vector<char>>(size, vector<char>(size,'_' ));
+//      };
+
+//          void printGrid() {
+//         for (int i = 0; i < size; ++i) {
+//             for (int j = 0; j < size; ++j) {
+//                 cout << grid[i][j] << " ";
+//             }
+//             cout << endl;
+//         }
+//     }
+
+//      void setGameGrid(){
+//      int size;
+//      cout<<"Enter the size of grid"<<endl;
+//      cin>>size;
+//       char *gameGrid=new char[size];
+//      }
+
+
+//      protected:
+
+// };
+
+
+
+
+// int main(){
+
+//    GameArea g1;
+//    g1.printGrid();
+
+
+
+
+//    return 0;
+// }
+
+
+
 #include<iostream>
-#include<string>
-#include<vector>
+#include<ctime>
 using namespace std;
-
-
-class GameArea{
-     private:
-     vector<vector<char>> grid;
-     int size;
-     char gameGrid[];
-     public:
-     GameArea(){
-      cout<<"Enter the size of area"<<endl;
-      cin>>size;
-      grid=vector<vector<char>>(size, vector<char>(size,'_' ));
-     };
-
-         void printGrid() {
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                cout << grid[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
-
-     void setGameGrid(){
-     int size;
-     cout<<"Enter the size of grid"<<endl;
-     cin>>size;
-      char *gameGrid=new char[size];
-     }
-
-
-     protected:
-
-};
-
-
-
-
-
-
-
-
-
-
-
 int main(){
+const int size=10;
+int randomArray[size]={1,4,5,7,6,9,2,-1,3,8};
 
-   GameArea g1;
-   g1.printGrid();
+    srand(time(0));
+    int random_number = rand() % 10; 
+    
+
+for(int i=0;i<size;i++){
+     if()
+
+
+}
 
 
 
 
-   return 0;
+
+    return 0;
 }
