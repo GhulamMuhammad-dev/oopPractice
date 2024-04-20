a0be9448fa165164897105fbfddb7bb4fc27b616
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-04-20 22:29:22 +0500

    The second commit of today

--- program.cpp
+++ program.cpp
@@ -1,107 +1,49 @@
-// #include<iostream>
-// #include<string>
-// #include<vector>
-// using namespace std;
-
-
-// class GameArea{
-//      private:
-//      vector<vector<char>> grid;
-//      int size;
-//      char gameGrid[];
-//      public:
-//      GameArea(){
-//       cout<<"Enter the size of area"<<endl;
-//       cin>>size;
-//       grid=vector<vector<char>>(size, vector<char>(size,'_' ));
-//      };
-
-//          void printGrid() {
-//         for (int i = 0; i < size; ++i) {
-//             for (int j = 0; j < size; ++j) {
-//                 cout << grid[i][j] << " ";
-//             }
-//             cout << endl;
-//         }
-//     }
-
-//      void setGameGrid(){
-//      int size;
-//      cout<<"Enter the size of grid"<<endl;
-//      cin>>size;
-//       char *gameGrid=new char[size];
-//      }
-
-
-//      protected:
-
-// };
-
-
-
-
-// int main(){
-
-//    GameArea g1;
-//    g1.printGrid();
-
-
-
-
-//    return 0;
-// }
-
-
-
 #include<iostream>
-#include<ctime>
+#include<string>
 using namespace std;
+
+class ShopItem{
+    public:
+     virtual int price()=0;
+
+};
+
+class Floar:public ShopItem{
+    public:
+    int price() override{
+      return 2000;
+    }
+};
+class Rice:public ShopItem{
+    public:
+    int price() override{
+      return 7000;
+    }
+};
+class Oil:public ShopItem{
+    public:
+    int price() override{
+      return 10000;
+    }
+};
+
+
+
+
+
+
+
 int main(){
 
-    int originalArray[] = {3, 7, 1, 9, 5, 2, 6, 8, 4, 10};
-    int target = 5;
-
-    int countLessThan = 0;
-    int countGreaterThan = 0;
-
-      for (int i = 0; i < sizeof(originalArray) / sizeof(originalArray[0]); ++i) {
-        if (originalArray[i] < target) {
-            ++countLessThan;
-        } else if (originalArray[i] > target) {
-            ++countGreaterThan;
-        }
-    }
-
-    int* lessThanTarget = new int[countLessThan];
-    int* greaterThanTarget = new int[countGreaterThan];
-
-     int lessThanIndex = 0;
-    int greaterThanIndex = 0;
 
 
-        for (int i = 0; i < sizeof(originalArray) / sizeof(originalArray[0]); ++i) {
-        if (originalArray[i] < target) {
-            lessThanTarget[lessThanIndex++] = originalArray[i];
-        } else if (originalArray[i] > target) {
-            greaterThanTarget[greaterThanIndex++] = originalArray[i];
-        }
-    }
-
-    std::cout << "Elements less than " << target << ": ";
-    for (int i = 0; i < countLessThan; ++i) {
-        std::cout << lessThanTarget[i] << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "Elements greater than " << target << ": ";
-    for (int i = 0; i < countGreaterThan; ++i) {
-        std::cout << greaterThanTarget[i] << " ";
-    }
-    std::cout << std::endl;
 
 
-      delete[] lessThanTarget;
-    delete[] greaterThanTarget;
+
+
+
+
+
 
     return 0;
 }
