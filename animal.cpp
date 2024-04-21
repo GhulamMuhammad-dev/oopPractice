677e7005c57c46c2b4e070bf036cadb6bfcade0e
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-04-21 23:31:15 +0500

    The third commit of today

--- animal.cpp
+++ animal.cpp
@@ -1,13 +1,15 @@
-// #include "animal.h"
+#include "animal.h"
 #include<iostream>
 using namespace std;
-// using namespace ad;
-// Animal::Animal(){
-//     cout<<"Animal constructor is called"<<endl;
-// }
+using namespace ad;
+Animal::Animal(){
+    cout<<"Animal constructor is called"<<endl;
+}
+Animal::~Animal(){cout<<"Destructor is called"<<endl;}
+
 int main(){
 
-// Animal a1;
-cout<<"Hello world"<<endl;
+Animal a1;
+
 
 }
