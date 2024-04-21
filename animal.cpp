e553845d24a50adf1efd653df5aad0a3f146e222
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-04-21 23:41:51 +0500

    The fourth commit of today

--- animal.cpp
+++ animal.cpp
@@ -5,11 +5,26 @@ using namespace ad;
 Animal::Animal(){
     cout<<"Animal constructor is called"<<endl;
 }
+
+void Animal::setType(string type){
+    this->type=type;
+}
+void Animal::setlegs(int legs){
+    this->legs=legs;
+}
+void Animal::showData(){
+    cout<<type<<":"<<endl;
+    cout<<legs<<":"<<endl;
+}
 Animal::~Animal(){cout<<"Destructor is called"<<endl;}
 
 int main(){
 
 Animal a1;
+
+a1.setlegs(4);
+a1.setType("dog");
+a1.showData();
 
 
 }
