ad534ec4cf3b6509df862889178daebfc8a350ef
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-04-23 23:19:28 +0500

    The third commit of today

--- animal.cpp
+++ animal.cpp
@@ -8,6 +8,31 @@ Calculator::Calculator(int fN,int sN,int op):firstNum(fN),secondNum(sN),oper(op)
 cout<<"constructor is called"<<endl;
 };
 
+double Calculator::calculate(){
+    double result;
+    switch (oper)
+    {
+    case '+':
+        result=firstNum+secondNum;
+        break;
+    case '-':
+        result=firstNum-secondNum;
+        break;
+    case '*':
+        result=firstNum*secondNum;
+        break;
+    case '/':
+        result=firstNum/secondNum;
+        break;
+    
+    default:
+    cout<<"wrong operator is used"<<endl;
+        break;
+    }
+
+    return result;
+}
+
 Calculator::~Calculator(){
     cout<<"Destructor is called"<<endl;
 }
