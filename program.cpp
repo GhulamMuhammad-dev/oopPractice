f1e221e8955f9e102e57696b0314206016b6ec0d
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-05-20 22:34:26 +0500

    The third commit of today

--- program.cpp
+++ program.cpp
@@ -1,40 +1,82 @@
+// #include<iostream>
+// using namespace std;
+
+// class Binary{
+//       private:
+//        float value;
+//      public:
+//      Binary(){};
+//        Binary(float v){
+//         value=v;
+//        }
+      
+//       void getValue(){
+//         cout<<"The value is:"<<this->value<<endl;
+//       }
+       
+//     Binary operator+(const Binary &other)const{
+//         Binary temp;
+//         temp.value=this->value+other.value;
+//         return temp;
+//     }
+//     Binary operator-(const Binary &other)const{
+//         Binary temp;
+//         temp.value=this->value-other.value;
+//         return temp;
+//     }
+//     Binary operator/(const Binary &other)const{
+//         Binary temp;
+//         temp.value=this->value/other.value;
+//         return temp;
+//     }
+//     Binary operator*(const Binary &other)const{
+//         Binary temp;
+//         temp.value=this->value*other.value;
+//         return temp;
+//     }
+     
+
+
+// };
+
+
+// int main(){
+
+// Binary b1(4.8),b2(8.5),res;
+// res=b1-b2;
+// res.getValue();
+
+
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
 
-class Binary{
-      private:
-       float value;
+
+class Complex{
+     private:
+     int real;
+     int img;
      public:
-     Binary(){};
-       Binary(float v){
-        value=v;
-       }
-      
-      void getValue(){
-        cout<<"The value is:"<<this->value<<endl;
-      }
-       
-    Binary operator+(const Binary &other)const{
-        Binary temp;
-        temp.value=this->value+other.value;
-        return temp;
+     Complex(){};
+     Complex(int real,int img){
+       this->real=real;
+       this->img=img;
+     }
+     void showValues(){
+        cout<<"( "<<real<<","<<img<<" )"<<"\n";
+        
+            }
+
+       Complex operator+(const Complex &other)const{
+       Complex temp;
+       temp.real=this->real+other.real;
+       temp.img=img+other.img;
+       return temp;
     }
-    Binary operator-(const Binary &other)const{
-        Binary temp;
-        temp.value=this->value-other.value;
-        return temp;
-    }
-    Binary operator/(const Binary &other)const{
-        Binary temp;
-        temp.value=this->value/other.value;
-        return temp;
-    }
-    Binary operator*(const Binary &other)const{
-        Binary temp;
-        temp.value=this->value*other.value;
-        return temp;
-    }
-     
+
+
 
 
 };
@@ -47,14 +89,10 @@ class Binary{
 
 int main(){
 
-Binary b1(4.8),b2(8.5),res;
-res=b1-b2;
-res.getValue();
 
-
-
-
-
+Complex c1(4,5),c2(5,5),res;
+res=c1+c2;
+res.showValues();
 
     return 0;
 }
