52fe0ec93b4cc9d8133f0f3e1bc7869efad484cb
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-05-28 23:17:41 +0500

    The third commit of today

--- program.cpp
+++ program.cpp
@@ -1,21 +1,62 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric> 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <numeric> 
 
-int main() {
-    std::vector<int> vec = {1, 2, 3, 4, 5};
+// int main() {
+//     std::vector<int> vec = {1, 2, 3, 4, 5};
 
   
-    std::sort(vec.begin(), vec.end());
+//     std::sort(vec.begin(), vec.end());
 
-    auto it = std::find(vec.begin(), vec.end(), 3);
-    if (it != vec.end()) {
-        std::cout << "Element found: " << *it << std::endl;
-    }
+//     auto it = std::find(vec.begin(), vec.end(), 3);
+//     if (it != vec.end()) {
+//         std::cout << "Element found: " << *it << std::endl;
+//     }
     
-    int sum = std::accumulate(vec.begin(), vec.end(), 0);
-    std::cout << "Sum of elements: " << sum << std::endl;
+//     int sum = std::accumulate(vec.begin(), vec.end(), 0);
+//     std::cout << "Sum of elements: " << sum << std::endl;
+
+//     return 0;
+// }
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Monster{
+     private:
+     int health;
+     int Attack;
+     
+
+     
+
+
+
+
+
+
+
+};
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
