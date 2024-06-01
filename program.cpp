11e42f82e0a8855e18cc0e262e0447cb89f5c98e
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-06-01 23:55:28 +0500

    The third commit of today

--- program.cpp
+++ program.cpp
@@ -1,41 +1,32 @@
-#include<iostream>
-#include<string> 
-using namespace std;
+#include <iostream>
+#include <string>
 
-class Animal{
-        private:
-        string name;
-        int health;
-        public:
+class Player {
+private:
+    std::string name;
+    int health;
+    int x, y; // Player's position in the grid
 
-        void setdata(string na,int h){
-              name=na;
-              health=h;
+public:
+    Player(const std::string& name, int health)
+        : name(name), health(health), x(0), y(0) {}
+
+    void move(char direction) {
+        switch (direction) {
+            case 'w': y--; break;
+            case 's': y++; break;
+            case 'a': x--; break;
+            case 'd': x++; break;
         }
-     
-       void getData() const{
-        cout<<name<<" "<<health<<endl;
-       }
+    }
 
+    void takeDamage(int damage) {
+        health -= damage;
+        if (health < 0) health = 0;
+    }
 
-
-
+    int getHealth() const { return health; }
+    int getX() const { return x; }
+    int getY() const { return y; }
+    std::string getName() const { return name; }
 };
-
-
-
-
-
-int main(){
-
- Animal dog;
-
-dog.setdata("dog",100);
-dog.getData();
-   
-
-
-
-
-  return 0;
-}
